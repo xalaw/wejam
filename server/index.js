633d0172981bdb193d76bec47180ef1bf9b0f243
Alvin Law <25409424+xalaw@users.noreply.github.com>
2018-02-19 15:11:57 -0800

    update runServer call.

--- server/index.js
+++ server/index.js
@@ -195,7 +195,7 @@ function closeServer() {
 }
 
 if (require.main === module) {
-  runServer(process.env.DATABASE_URL, process.env.PORT).catch(err => console.error(err));
+  runServer(DATABASE_URL, PORT).catch(err => console.error(err));
 }
 
 module.exports = {
